45e82e4c27459d018912f05484c6734c2476d4d8
Wolfrein <tigr5557@gmail.com>
2022-04-11 20:40:52 +0300

    Create example "using delegate"

---
+++ Using delegate/Calculate.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Using_delegate
+{
+    //Create delegate
+    public delegate void Notification(string messege);
+    internal class Calculate
+    {
+        //Declarete delegate
+        Notification? notification;
+        // Add to delegete metod
+        public void RegistrMetod(Notification del)
+        {
+            notification += del;
+        }
+        public int Add(int x, int y)
+        {
+            int result = x + y;
+            notification?.Invoke($"Sum add: {result}"); //Call delegate
+            return result;
+        }
+        public int Subtraction(int x, int y)
+        {
+            int result = x - y;
+            notification?.Invoke($"Sum Subtraction: {result}"); //Call delegate
+            return result;
+        }
+        public int Multiply(int x, int y)
+        {
+            int result = x * y;
+            notification?.Invoke($"Sum Multiply: {result}"); //Call delegate
+            return result;
+        }
+
+        public double Dividsion(double x, double y)
+        {
+            try //Checking division by 0
+            {
+                double result = x / y;
+                notification?.Invoke($"Sum Dividsion: {result}"); //Call delegate
+                return result;
+            }
+            catch (DivideByZeroException) 
+            {
+                notification?.Invoke("Attempt to divide by zero"); //Call delegate
+                return 0;
+            }
+
+        }
+    }
+}
---
+++ Using delegate/Program.cs
@@ -0,0 +1,17 @@
+﻿using Using_delegate; 
+
+Calculate calculate = new Calculate();
+calculate.RegistrMetod(PrintNotificatoin); //Add metod to delegate
+calculate.Add(-1, 5);
+calculate.RegistrMetod(PrintColorText); //Add metod to delegate
+calculate.Multiply(3, 5);
+calculate.Dividsion(46, 87);
+
+
+void PrintNotificatoin(string messege) => Console.WriteLine(messege);
+void PrintColorText(string messege) 
+{
+    Console.ForegroundColor = ConsoleColor.Blue;
+    Console.WriteLine(messege);
+    Console.ResetColor();
+}
